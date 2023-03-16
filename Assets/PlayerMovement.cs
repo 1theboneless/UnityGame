28102f021a42f15b3fa812a7e2a5bf320141c3d5
lionex-ui <eugenelewinston@gmail.com>
2023-03-16 14:08:57 +0200

    rotation movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -10,11 +10,14 @@ public class PlayerMovement : MonoBehaviour
     public float jumpCooldown = 1f;
     public float jumpCooldownTimer = 0f;
 
-    public float rotationSpeed = 5f;
+    public float rotationTime = 0.1f;
+    float rotationVelocity;
 
     public bool canJump = true;
 
+    public CharacterController controller;
     public Transform characterTransform;
+    public Transform mainCamera;
     public Rigidbody rb;
 
     // Start is called before the first frame update
@@ -26,17 +29,18 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput).normalized;
 
-        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-        movementDirection.Normalize();
-
-        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
-
-        if (movementDirection != Vector3.zero)
+        if (direction.magnitude >= 0.1f)
         {
-            transform.forward = movementDirection;
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + mainCamera.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationVelocity, rotationTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
 
         if (Input.GetButtonDown("Jump") && canJump)
