7091ce28a64ae84dcee6738140b269dbdb5fd643
1theboneless <106874694+1theboneless@users.noreply.github.com>
2023-03-16 12:08:18 +0200

    fdhg

---
+++ Assets/ThirdPersonCam.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCam : MonoBehaviour
+{
+    [Header("References")]
+    public Transform orientation;
+    public Transform player;
+    public Transform PlayerObj;
+    public Rigidbody rb;
+
+    public float rotationSpeed;
+}
