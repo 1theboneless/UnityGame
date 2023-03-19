a1ab863f065e9fb09335a1b75d39379369a6ed57
1theboneless <106874694+1theboneless@users.noreply.github.com>
2023-03-20 00:38:05 +0200

    FixedMovement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,18 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10f;
-
     public float jumpForce = 5f;
     public float jumpCooldown = 1f;
     public float jumpCooldownTimer = 0f;
-
     public float rotationTime = 0.1f;
-    float rotationVelocity;
+    private float rotationVelocity;
+    private bool canJump = true;
 
-    public bool canJump = true;
-
-    public CharacterController controller;
-    public Transform characterTransform;
     public Transform mainCamera;
     public Rigidbody rb;
 
@@ -24,26 +19,23 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        if (rb == null)
+        {
+            Debug.LogError("Rigidbody component not found on player object.");
+        }
 
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main.transform;
+        }
+
+        rb.isKinematic = false;
+        canJump = true; // set canJump to true in the Start method
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput).normalized;
-
-        if (direction.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + mainCamera.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationVelocity, rotationTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
-        }
-
         if (Input.GetButtonDown("Jump") && canJump)
         {
             Jump();
@@ -59,9 +51,42 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    // FixedUpdate is called at a fixed interval and is synchronized with the physics engine
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + mainCamera.eulerAngles.y;
+            if (float.IsNaN(targetAngle))
+            {
+                targetAngle = 0f;
+            }
+
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationVelocity, rotationTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            rb.MovePosition(rb.position + moveDir.normalized * speed * Time.fixedDeltaTime);
+        }
+    }
+
+    // LateUpdate is called once per frame after all other Update functions have been called
+    void LateUpdate()
+    {
+        // update the position and rotation of the character model or camera
+        transform.position = rb.position;
+        // assuming the camera is a child of the character, update its rotation relative to the character's rotation
+        mainCamera.localRotation = Quaternion.Euler(mainCamera.localEulerAngles.x, transform.localEulerAngles.y, mainCamera.localEulerAngles.z);
+    }
+
     private void Jump()
     {
         rb.velocity = Vector3.up * jumpForce;
+        rb.isKinematic = false;
         canJump = false;
         jumpCooldownTimer = jumpCooldown;
     }
